5307361c793c0c7fbb87415b8d17c1e6947cf0a4
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-06-26 06:17:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+console.log(destructivelyAppendCat);
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+
+
